5f708416618171b65c440f8ff5635ff92d231583
bfazeli <bfazeli1@gmail.com>
2018-12-09 18:10:30 -0800

    Update: Added http dependency and created a class to keep track of the state of the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'strings.dart';
 
 void main() => runApp(new GHFlutterApp());
 
@@ -7,15 +12,47 @@ class GHFlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'GHFlutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('GHFlutter App'),
-        ),
-        body: new Center(
-          child: new Text('GHFlutter'),
-        ),
-      ),
+      title: Strings.appTitle,
+      home: new GHFlutter(),
     );
   }
+}
+
+var _members = [];
+final _biggerFont = const TextStyle(fontSize: 18.0);  // _ makes these members private accessors
+
+
+class GHFlutterState extends State<GHFlutter> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(Strings.appTitle),
+      ),
+      body: new Text(Strings.appTitle),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    
+    _loadData();
+  }
+
+  _loadData() async {
+    String dataURL = "https://github.com/orgs/codeandcoffeelb/people";
+    // http.Response response = await http.get(dataURL);
+    http.get(dataURL)
+    .then( (response) => 
+      setState(() {
+        _members = jsonDecode(response.body);
+      })
+    );
+  }
+}
+
+class GHFlutter extends StatefulWidget {
+  @override
+  createState() => new GHFlutterState();
 }
