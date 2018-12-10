e1885fdcaa70f0562faecba4bfe69f231f395ec2
bfazeli <bfazeli1@gmail.com>
2018-12-09 23:30:27 -0800

    Update: added dividers and parsed response to custom type

--- lib/main.dart
+++ lib/main.dart
@@ -18,27 +18,21 @@ class GHFlutterApp extends StatelessWidget {
   }
 }
 
-var _members = [];
+var _members = <Member>[];
 final _biggerFont = const TextStyle(fontSize: 18.0);  // _ makes these members private accessors
 
+class Member {
+  final String login;
+
+  Member(this.login) {
+    if (login == null) {
+      throw new ArgumentError("login of member cannot be null."
+        "Received: '$login'");
+    }
+  }
+}
 
 class GHFlutterState extends State<GHFlutter> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(Strings.appTitle),
-      ),
-      body: new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemCount: _members.length,
-        itemBuilder: (BuildContext context, int position){
-          return _buildRow(position);
-        },
-      ),
-    );
-  }
-
   @override
   void initState() {
     super.initState();
@@ -46,9 +40,34 @@ class GHFlutterState extends State<GHFlutter> {
     _loadData();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(Strings.appTitle),
+      ),
+      body: new ListView.builder(
+        itemCount: _members.length * 2,
+        itemBuilder: (BuildContext context, int position){
+          if (position.isOdd) {
+            return new Divider();
+          }
+
+          final index = position ~/ 2;
+
+          return _buildRow(index);
+        },
+      ),
+    );
+  }
+
+
   Widget _buildRow(int i) {
-    return new ListTile(
-      title: new Text("${_members[i]["login"]}", style: _biggerFont),
+    return new Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: new ListTile (
+        title: new Text("${_members[i].login}", style: _biggerFont)
+      )
     );
   }
 
@@ -59,7 +78,11 @@ class GHFlutterState extends State<GHFlutter> {
     http.get(dataURL)
     .then( (response) => 
       setState(() {
-        _members = jsonDecode(response.body);
+        final membersJSON = jsonDecode(response.body);
+        for (var memberJSON in membersJSON) {
+          final member = new Member(memberJSON["login"]);
+          _members.add(member);
+        }
       })
     );
   }
