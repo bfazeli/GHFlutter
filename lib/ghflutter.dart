f23501ea0141d03b496575dcd979833f77999741
bfazeli <bfazeli1@gmail.com>
2018-12-11 18:54:50 -0800

    Update: Made a custom animation to show the details of the user.

--- lib/ghflutter.dart
+++ lib/ghflutter.dart
@@ -18,13 +18,34 @@ class GHFlutterState extends State<GHFlutter> {
     _loadData();
   }
 
+  // _pushMember(Member member) {
+  //     Navigator.of(context).push(
+  //       new MaterialPageRoute(
+  //         builder: (context) => new MemberWidget(member)
+  //       )
+  //     );
+  //   }
+
   _pushMember(Member member) {
-      Navigator.of(context).push(
-        new MaterialPageRoute(
-          builder: (context) => new MemberWidget(member)
-        )
-      );
-    }
+    Navigator.of(context).push(
+      new PageRouteBuilder(
+        opaque: true,
+        transitionDuration: const Duration(milliseconds: 1000),
+        pageBuilder: (BuildContext context, _, __) {
+          return new MemberWidget(member);
+        },
+        transitionsBuilder: (_, Animation<double> animation, __, Widget child) {
+          return new FadeTransition(
+            opacity: animation,
+            child: new RotationTransition(
+              turns: new Tween<double>(begin: 0.0, end: 1.0).animate(animation),
+              child: child,
+            ),
+          );
+        }
+      )
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
