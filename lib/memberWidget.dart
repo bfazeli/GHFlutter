3e124efffc6fda7e8ad826f62546ac7a437758fa
bfazeli <bfazeli1@gmail.com>
2018-12-11 17:46:11 -0800

    Update: Detected and returned a value from the child stack to the parent

--- lib/memberWidget.dart
+++ lib/memberWidget.dart
@@ -14,8 +14,56 @@ class MemberState extends State<MemberWidget> {
       ),
       body: new Padding(
         padding: new EdgeInsets.all(16.0),
-        child: new Image.network(member.avatarUrl),
+        child: new Column(
+          children: <Widget>[
+            new Image.network(member.avatarUrl),
+            new IconButton(
+              icon: new Icon(Icons.arrow_back, color: Colors.green, size: 48.0),
+              onPressed: () { Navigator.pop(context); },
+            ),
+            new RaisedButton(
+              child: new Text('PRESS ME'),
+              onPressed: () {_showOKScreen(context);},
+            )
+          ],
+        )
       ),
+    );
+  }
+
+  _showOKScreen(BuildContext context) async {
+    bool value = await Navigator.of(context).push(new MaterialPageRoute<bool>(
+      builder: (BuildContext context) {
+        return new Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: new Column(
+            children: <Widget>[
+              new GestureDetector(
+                child: new Text("OK"),
+                onTap: () {Navigator.of(context).pop(true);},
+              ),
+              new GestureDetector(
+                child: new Text("NOT OK"),
+                onTap: () {Navigator.of(context).pop(false);},
+              )
+            ],
+          ),
+        );
+      }
+    ));
+    
+    var alert = new AlertDialog(
+      content: new Text((value != null && value) ? "OK was pressed" : "NOT OK or BACK was pressed"),
+      actions: <Widget>[
+        new FlatButton(
+          child: new Text('OK'),
+          onPressed: () {Navigator.of(context).pop();},
+        )
+      ],
+    );
+    showDialog(
+      context: context,
+      builder: (_) => alert
     );
   }
 }
